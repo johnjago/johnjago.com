ef9f19b41afbe225c37825da71a5d37d8527cfef
John Jago <johnjago@protonmail.com>
2018-12-22 14:34:27 -0600

    Add some content and create a deploy script

---
+++ README.md
@@ -0,0 +1,7 @@
+# johnjago.com
+
+Plain HTML and CSS, along with a script to do the dirty work.
+
+# License
+
+CC0
---
+++ src/main.js
@@ -0,0 +1,8 @@
+function clickAndUpdate(id, text) {
+  const $el = document.getElementById(id);
+  $el.addEventListener('click', e => {
+    e.target.textContent = text;
+  })
+}
+
+clickAndUpdate('email', 'johnjago@' + 'protonmail.com')
