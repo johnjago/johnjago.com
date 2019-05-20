d53aa964c98f475806cfbfb9c14c00707ac784f3
John Jago <johnjago@protonmail.com>
2019-05-19 19:52:09 -0500

    Remove deprecated deploy scripts

--- src/blog/up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-hugo
-rsync -av --delete public/ do:/var/www/html/johnjago.com/public/blog/
-rm -rf public/
--- up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-html-minifier src/index.html -o dist/index.html --collapse-whitespace
-sass src/*.css dist/style.css --no-source-map -s compressed
-cp src/main.js dist/main.js
-scp dist/* do:/var/www/html/johnjago.com/public/
