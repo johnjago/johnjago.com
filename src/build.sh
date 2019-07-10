76bbea026b63f1e30a9d45a53c1ca3c748c5c418
John Jago <johnjago@protonmail.com>
2019-07-10 18:29:40 -0500

    Add hash to CSS filename for cache busting

---
+++ src/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE='style.css'
+# md5sum outputs the hash followed by the filename.
+# Only the hash part is needed, so wrap the result in an array
+# and index the first element in the array by the name.
+# e.g. HASH and HASH[0] refer to the start of the array.
+HASH=($(md5sum $FILE))
+FILE_WITH_HASH=style.$HASH.css
+cp $FILE $FILE_WITH_HASH
+sed -i "s/style[.0-9a-z]*.css/$FILE_WITH_HASH/g" index.html
