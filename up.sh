5592fe768c37bc9961f8d9869229243b5855f928
John Jago <johnjago@protonmail.com>
2021-01-21 16:36:57 -0600

    Remove Travis CI config and replace with script
    travis-ci.org is shutting down, plus it takes 5+ minutes to actually
    start building whereas this script updates the site in less than a
    second.

---
+++ up.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Remove old temporary directories if present
+rm -r tmp/ dist/
+
+# Create temporary build directories
+cp -R src tmp/
+mkdir dist/
+
+# Run the build for the Hugo blog, then go up to project site dir
+cd tmp/blog/ && hugo && mv public ../../dist/blog/ && cd ..
+
+# Insert hash into project site CSS filename for cache busting
+./build.sh
+
+# Move project site into dist/
+rsync -av \
+    --exclude=blog/ \
+    --exclude=style.css \
+    --exclude=build.sh \
+    . ../dist/ && cd ..
+
+# Upload to server
+rsync -rP --delete \
+    --exclude=/deformat \
+    --exclude=/musearch \
+    dist/ root@johnjago.com:/var/www/johnjago.com/
+
+# Clean up temporary directories, leaving dist/ for debugging
+rm -r tmp/
