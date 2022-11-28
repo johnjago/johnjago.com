2c06dc193cbf49f7cdd6cad44953b1d185ed3582
John Jago <jc@johnjago.com>
2022-11-27 22:50:06 -0600

    build: Use BSD sed and md5 for macOS

--- src/build.sh
+++ src/build.sh
@@ -5,7 +5,8 @@ FILE='style.css'
 # Only the hash part is needed, so wrap the result in an array
 # and index the first element in the array by the name.
 # e.g. HASH and HASH[0] refer to the start of the array.
-HASH=($(md5sum $FILE))
+HASH=($(md5 -q $FILE))
 FILE_WITH_HASH=style.$HASH.css
 cp $FILE $FILE_WITH_HASH
-sed -i "s/style[.0-9a-z]*.css/$FILE_WITH_HASH/g" index.html
+sed -i'.bak' -e "s/style[.0-9a-z]*.css/$FILE_WITH_HASH/g" index.html
+rm index.html.bak
